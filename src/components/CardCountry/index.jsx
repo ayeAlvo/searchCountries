611df284aa666513ea6171ca8985823d9a2650d6
ayeAlvo <ayealvo@gmail.com>
2022-05-05 00:33:10 -0300

    cambios en el renderizado

--- src/components/CardCountry/index.jsx
+++ src/components/CardCountry/index.jsx
@@ -4,24 +4,22 @@ import "./index";
 const CardCountry = (props) => {
     return (
     <>
-    {props.country.length === 0 ? <h4 className="text-primary">try some text <i className="fa-solid fa-magnifying-glass-location text-primary p-2"></i></h4>
+  {props.country.length === 0 ? <h4 className="text-primary">try some text <i className="fa-solid fa-magnifying-glass-location text-primary p-2"></i></h4>
     :  <div className="mb-5 row">
-{props.country.map(country => <div className="col animate__animated animate__fadeIn" key={country.code}>    
+  {props.country.map(country => <div className="col animate__animated animate__fadeIn" key={country.code}>    
   <div className="card mb-3 border-primary shadow" style={{width: "18rem"}}>
   <div className="card-header bg-transparent">
-  {props.continent === true ? <h3 className="card-title">{country.continent.name}</h3>
-    : 
-    <h3 className="card-title">{country.languages.map(len => <p key={len.name}>{len.name}</p>)}</h3>
+  {props.continent ? <h3 className="card-title">{country.continent.name}</h3>
+    : <h3 className="card-title">{country.languages.map(len => <p key={len.name}>{len.name}</p>)}</h3>
     }
   </div>
     <div className="card-body text-primary">
-      <h5 className="card-title">{country.emoji}  {country.name}</h5>
+      <h5 className="card-title">{country.emoji} - {country.name}</h5>
       <h6 className="card-subtitle mb-2 text-muted">{country.native}</h6>
-      {props.continent === true ? <ul className="card-title"><span className="">Languages:</span> {country.languages.map(len => <li className="text-muted" key={len.name}>{len.name}</li>)}</ul>
-    : 
-    <p className="card-title">Continent: <span className="text-muted">{country.continent.name}</span></p>
+      {props.continent ? <ul className="card-title"><span className="">Languages:</span> {country.languages.map(len => <li className="text-muted" key={len.name}>{len.name}</li>)}</ul>
+    : <p className="card-title">Continent: <span className="text-muted">{country.continent.name}</span></p>
     }
-    <p className="card-text">Capital: <span className="text-muted">{country.capital}</span></p>
+    {country.capital ? <p className="card-text">Capital: <span className="text-muted">{country.capital}</span></p> : ""}
     </div> 
   </div>
 </div> )}
--- src/components/ContentWrapper/index.jsx
+++ src/components/ContentWrapper/index.jsx
@@ -10,7 +10,7 @@ const ContentWrapper = () => {
     <div className="container shadow bg_color">
       <h2 className="text-start mt-md-5 mt-2 fs-1 text-primary animate__animated animate__headShake">Country Search</h2>
       <h4 className="text-start mb-md-5 mb-4 mt-md-3 mt-2 text-primary">for letters and words</h4>
-      {loading ? <p>Loading...</p>
+      {loading ? <i className="fa-solid fa-spinner fa-spin-pulse fa-3x m-3 text-primary"></i>
       : <SearchCountry countries={data?.countries} />            
       }
       <Footer />
