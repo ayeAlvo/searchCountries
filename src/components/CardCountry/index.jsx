985b76d664fdc28a366973f5e6b601dcc8a6e1a6
ayeAlvo <ayealvo@gmail.com>
2022-05-05 22:01:17 -0300

    trato los resultados nulos en el card

--- src/components/CardCountry/index.jsx
+++ src/components/CardCountry/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./index";
 
 const CardCountry = (props) => {
+  console.log(props);
     return (
     <>
   {props.country.length === 0 ? <h4 className="text-primary">try some text <i className="fa-solid fa-magnifying-glass-location text-primary p-2"></i></h4>
@@ -9,6 +10,7 @@ const CardCountry = (props) => {
   {props.country.map(country => <div className="col animate__animated animate__fadeIn" key={country.code}>    
   <div className="card mb-3 border-primary shadow" style={{width: "18rem"}}>
   <div className="card-header bg-transparent">
+  {props.language && country.languages.length === 0 ? <h3 className="card-title"><span className="">Language not available</span></h3> : "" }
   {props.continent ? <h3 className="card-title">{country.continent.name}</h3>
     : <h3 className="card-title">{country.languages.map(len => <p key={len.name}>{len.name}</p>)}</h3>
     }
@@ -16,7 +18,7 @@ const CardCountry = (props) => {
     <div className="card-body text-primary">
       <h5 className="card-title">{country.emoji} - {country.name}</h5>
       <h6 className="card-subtitle mb-2 text-muted">{country.native}</h6>
-      {props.continent ? <ul className="card-title"><span className="">Languages:</span> {country.languages.map(len => <li className="text-muted" key={len.name}>{len.name}</li>)}</ul>
+      {props.language && country.languages.length > 0 ? <ul className="card-title"><span className="">Languages:</span> {country.languages.map(len => <li className="text-muted" key={len.name}>{len.name}</li>)}</ul>
     : <p className="card-title">Continent: <span className="text-muted">{country.continent.name}</span></p>
     }
     {country.capital ? <p className="card-text">Capital: <span className="text-muted">{country.capital}</span></p> : ""}
